df6a18ca13ce4bda4670d5a252249d09e0711a50
Stella Nguyen <stellakimnguyen@gmail.com>
2020-01-26 00:54:28 -0500

    Front-End setup

---
+++ screens/App.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.profileHeader}>
+        {/* <Image 
+          source={require('./../images/user_avatar.png')}  
+          style={{width: 90, height: 90, borderRadius: 90, marginRight: 20}} 
+        /> */}
+        <View>
+          <Text style={styles.username}>John Smith</Text>
+          <Text style={styles.level}>John Smith</Text>
+          <Text style={styles.points}>1002 pts</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FAFAFF',
+    paddingLeft: 30,
+    paddingTop: 50,
+  },
+
+  username: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    color: '#707070',
+    letterSpacing: 1,
+  },
+
+  level: {
+    fontSize: 16,
+    color: '#707070',
+    letterSpacing: 0.5,
+  },
+
+  points: {
+    fontSize: 18,
+    color: '#F6CB00',
+    letterSpacing: 0.5,
+    fontWeight: 'bold',
+  },
+
+  profileHeader: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+  },
+});
