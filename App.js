2ded7a0e4dcbf741099ed220f003a85e0e694110
Stella Nguyen <stellakimnguyen@gmail.com>
2020-01-25 23:53:17 -0500

    Add user icon

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Image 
+        source={require('./images/user_avatar.png')}  
+        style={{width: 400, height: 400, borderRadius: 400}} 
+      />
       <Text>Open up App.js to start working on your app!</Text>
     </View>
   );
@@ -15,5 +19,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    margin: 20,
   },
 });
